031f20baed13b6624e6909d6f32d5f83be91e03b
Fábio David Freitas <fabio.david.contato@gmail.com>
2021-10-26 15:04:36 -0300

    Started routing

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupPage from './pages/NewMeetup'
+import FavoritesPage from './pages/Favorites'
+
 
 function App() {
   return (
     <div className="App">
 
+      <Switch>
+      <Route path="/" exact>
+        <AllMeetupsPage />
+      </Route>
+
+      <Route path="/new-meetup">
+        <NewMeetupPage />
+      </Route>
+
+      <Route path="/favorites" exact>
+        <FavoritesPage />
+      </Route>
+      </Switch>
     </div>
   );
 }
